b05387568d934cfb9f760e13bf50b2d7b39d8af0
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-07 08:35:04 +0000

    lang/perl/WWW-MobileCarrierJP: added HTTP::MobileAgent::Flash::DoCoMoFlashMap checking script.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@2791 d0d07461-0603-4401-acd4-de1884942a52

---
+++ tools/check_http_mobileagent_flash_map.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+use FindBin;
+use File::Spec;
+use HTTP::MobileAgent::Flash::DoCoMoFlashMap qw/$FLASH_MAP/;
+
+use lib File::Spec->catfile($FindBin::Bin, '..', 'lib');
+use WWW::MobileCarrierJP::DoCoMo::Flash;
+
+my $dat = WWW::MobileCarrierJP::DoCoMo::Flash->scrape;
+
+for my $version (@$dat) {
+    for my $model (@{$version->{models}}) {
+        unless (exists $FLASH_MAP->{$model->{model}}) {
+            warn "orz: $model";
+        }
+    }
+}
