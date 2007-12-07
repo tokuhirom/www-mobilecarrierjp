dbda52d6310ac7b402c748beab710cef61411c08
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-07 06:48:34 +0000

    lang/perl/WWW-MobileCarrierJP/trunk/lib/WWW/MobileCarrierJP/ThirdForce/CIDR.pm: revert to r2699.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@2783 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP/ThirdForce/CIDR.pm
+++ lib/WWW/MobileCarrierJP/ThirdForce/CIDR.pm
@@ -8,7 +8,7 @@ my $url = 'http://developers.softbankmobile.co.jp/dp/tech_svc/web/ip.php';
 
 sub scrape {
     scraper {
-        process '//td[@bgcolor="#eeeeee"]/font[@size="2" and @class="j10" and @color!="#ff0000"]', 'cidr[]', ['TEXT', sub {
+        process '//td[@bgcolor="#eeeeee"]/font[@size="2" and @class="j10"]', 'cidr[]', ['TEXT', sub {
                         m{^([0-9.]+)(/[0-9]+)};
                         +{ ip => $1, subnetmask => $2 };
                     }];
