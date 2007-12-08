2a87025feeb4136bf5f1f3d893613e08875ce02f
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-08 03:52:43 +0000

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 406)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension WWW::MobileCarrierJP
     
    +0.03  Fri Dec  7 19:01:38 PST 2007
    +         - added docomo flash site scraper
    +         - added thirdforce user agent scraper
    +         - added ezweb gif/png/flash_lite/jpeg scraper
    +
     0.02  Thu Dec  6 21:32:20 PST 2007
             - added Pictogram Info scraper.
               copied from Encode-JP-Mobile.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@2856 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP.pm
+++ lib/WWW/MobileCarrierJP.pm
@@ -2,7 +2,7 @@ package WWW::MobileCarrierJP;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
---
+++ tools/dump_all.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use YAML;
+use FindBin;
+use File::Spec;
+use lib File::Spec->catfile($FindBin::Bin, '..', 'lib');
+use Module::Pluggable::Fast name => 'components', search => ['WWW::MobileCarrierJP'], require => 'yes';
+
+my $datdir = File::Spec->catfile($FindBin::Bin, '..', 'dat');
+mkdir $datdir;
+for my $module (components()) {
+    my $fname = $module;
+    $fname =~ s/^WWW::MobileCarrierJP:://;
+    $fname =~ s/::/-/g;
+    $fname = lc $fname;
+    YAML::DumpFile(
+        File::Spec->catfile($datdir, "$fname.yaml"),
+        $module->scrape(),
+    );
+}
+
