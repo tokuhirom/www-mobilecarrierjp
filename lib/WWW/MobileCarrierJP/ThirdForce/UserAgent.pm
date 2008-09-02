ffcc49d81013d52f2374c0450304c38e20695b69
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-02 15:43:05 +0000

    f*cking softbank--(why remake developers site????)
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@18678 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
+++ lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
@@ -1,40 +1,17 @@
 package WWW::MobileCarrierJP::ThirdForce::UserAgent;
 use strict;
 use warnings;
+use WWW::MobileCarrierJP::Declare;
 use URI;
-use Web::Scraper;
 
-my @urls = map { URI->new($_) } map {
-    "http://developers.softbankmobile.co.jp/dp/tech_svc/info/useragent.php?page=$_"
-} ( 1 .. 4 );
-
-sub scrape {
-    my $s = scraper {
-        process '//table[@width="100%"]/tr[position()>2]', 'models[]', scraper {
-            process 'td:nth-child(1)', 'model', 'TEXT';
-            process 'td:nth-child(2)', 'user_agent', 'TEXT';
-            process 'td:nth-child(3)', 'msname', 'TEXT';
-            process 'td:nth-child(4)', 'display', ['TEXT', sub {
-                /^(\d+)\*(\d+)$/; +{ width => $1, height => $2 };
-            }];
-            process 'td:nth-child(5)', 'is_color', ['TEXT', sub {
-                /^C/ ? 1 : 2
-            }];
-            process 'td:nth-child(5)', 'color', ['TEXT', sub {
-                s/^[CG]//;
-            }];
-            process 'td:nth-child(8)', 'java', ['TEXT', sub {
-                /1/ ? 1 : undef
-            }];
-        };
-    };
-
-    my $result;
-    for my $uri (@urls) {
-        push @$result, @{$s->scrape($uri)->{models}};
-    }
-    return [grep { $_->{model} !~ /series/ } @$result ];
-}
+parse_one( 
+    urls => ['http://creation.mb.softbank.jp/terminal/index.html'],
+    xpath => q(//tr[@bgcolor="#FFFFFF"]/td[@rowspan="4"]/..),
+    scraper => scraper {
+        col 1 => 'model'      => 'TEXT';
+        col 2 => 'user_agent' => 'TEXT';
+    },
+);
 
 1;
 __END__
--- t/07_thirdforce_useragent.t
+++ t/07_thirdforce_useragent.t
@@ -1,19 +1,17 @@
 use strict;
 use warnings;
-use Test::More tests => 11;
+use Test::More tests => 6;
 use WWW::MobileCarrierJP::ThirdForce::UserAgent;
 
 my $dat = WWW::MobileCarrierJP::ThirdForce::UserAgent->scrape;
 ok ref($dat), 'ARRAY';
 cmp_ok scalar(@$dat), '>', 30;
 
-is $dat->[0]->{model}, 'V403SH';
-is $dat->[0]->{user_agent}, 'J-PHONE/3.0/V403SH';
-is $dat->[0]->{msname}, 'V403SH';
-is $dat->[0]->{display}->{width}, 240;
-is $dat->[0]->{display}->{height}, 260;
-is $dat->[0]->{is_color}, 1;
-is $dat->[0]->{color}, 262144;
+{
+    my ($m, ) = grep { $_->{model} eq '823T' } @$dat;
+    ok $m;
+    is $m->{user_agent}, 'SoftBank/1.0/823T/TJ001[/Serial] Browser/NetFront/3.3 Profile/MIDP-2.0 Configuration/CLDC-1.1';
+}
 
 ok grep { $_->{model} eq '821P' } @$dat;
 is scalar(grep { $_->{model} =~ /series/ } @$dat), 0;
