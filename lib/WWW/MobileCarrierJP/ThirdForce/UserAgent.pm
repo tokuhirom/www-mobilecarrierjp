a19b7ffc8bc537dd5f8c01f044bf06d8dfcba33b
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-30 14:38:12 +0000

    obsolete
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@22392 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
+++ lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
@@ -15,12 +15,21 @@ __END__
 
 =head1 NAME
 
-WWW::MobileCarrierJP::ThirdForce::UserAgent - get UserAgent informtation from ThirdForce site.
+WWW::MobileCarrierJP::ThirdForce::UserAgent - get UserAgent informtation from ThirdForce site.(OBSOLETE)
 
 =head1 SYNOPSIS
 
-    use WWW::MobileCarrierJP::ThirdForce::UserAgent;
-    WWW::MobileCarrierJP::ThirdForce::UserAgent->scrape();
+    (OBSOLETE)
+
+=head1 DESCRIPTION
+
+THIS MODULE IS OBSOLETE.
+
+YOU SHOULD USE ThirdForce::HTTPHeader & ThirdForce::Service.
+
+This module is no longer supported.
+
+You should also strongly avoid the use of this module.
 
 =head1 AUTHOR
 
