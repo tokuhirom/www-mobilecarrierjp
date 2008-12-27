7e210e49c8726ea7a34894591a0019d8325c5ae0
kan <kan@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-27 14:32:13 +0000

    fixed willcom cidr info.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@27488 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP.pm
+++ lib/WWW/MobileCarrierJP.pm
@@ -2,7 +2,7 @@ package WWW::MobileCarrierJP;
 use strict;
 use warnings;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 1;
 
--- lib/WWW/MobileCarrierJP/AirHPhone/CIDR.pm
+++ lib/WWW/MobileCarrierJP/AirHPhone/CIDR.pm
@@ -4,11 +4,11 @@ use warnings;
 use Web::Scraper;
 use URI;
 
-sub url { 'http://www.willcom-inc.com/ja/service/contents_service/club_air_edge/for_phone/ip/' }
+sub url { 'http://www.willcom-inc.com/ja/service/contents_service/create/center_info/index.html' }
 
 sub scrape {
     scraper {
-        process '//td[@width="50%"]/font[@size="2"]', 'cidr[]', ['TEXT', sub {
+        process '//td[@align="center" and @bgcolor="white"]/font[@size="2"]', 'cidr[]', ['TEXT', sub {
                         m{^([0-9.]+)(/[0-9]+)};
                         +{ ip => $1, subnetmask => $2 };
                     }];
