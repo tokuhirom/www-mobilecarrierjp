4b4f2d64f37e682525a1e9bdd10c5a6660ee22e2
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-20 05:15:28 +0000

    use Exporter instead of Sub::Exporter.because that is more popular ;)
    git-svn-id: http://svn.coderepos.org/share/lang/perl/WWW-MobileCarrierJP/trunk@30338 d0d07461-0603-4401-acd4-de1884942a52

--- lib/WWW/MobileCarrierJP/Declare.pm
+++ lib/WWW/MobileCarrierJP/Declare.pm
@@ -2,23 +2,20 @@ package WWW::MobileCarrierJP::Declare;
 use strict;
 use warnings;
 use utf8;
+use base qw/Exporter/;
 use Web::Scraper;
 use URI;
-use Sub::Exporter;
 
-my $exporter = Sub::Exporter::build_exporter(
-    {
-        exports => [qw(parse_one scraper process col as_tree)],
-        groups  => { default => [':all'] }
-    },
-);
+our @EXPORT = qw(parse_one scraper process col as_tree);
 
 sub import {
+    my $class = shift;
+
     strict->import;
     warnings->import;
     utf8->import;
 
-    goto $exporter;
+    $class->export_to_level(1);
 }
 
 sub col {
