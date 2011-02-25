d43ac6667692278d221f27facf969f927148eaa9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-26 00:51:50 +0900

    rewrite dumper script. new version dumps JSON.

--- tools/www-mobilecarrierjp-dump_all.pl
+++ tools/www-mobilecarrierjp-dump_all.pl
@@ -1,28 +1,59 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
-use YAML;
 use FindBin;
 use File::Spec;
 use lib File::Spec->catfile($FindBin::Bin, '..', 'lib');
-use Module::Pluggable::Object;
+use Module::Pluggable::Object; # TODO: switch to Module::Find
+use Getopt::Long;
+use autodie;
+use File::Path qw/mkpath/;
+use IO::Handle;
+use Pod::Usage;
+
+my $format = 'YAML';
+my $datdir = File::Spec->catfile($FindBin::Bin, '..', 'dat');
+GetOptions(
+    'format=s' => \$format,
+    'datdir=s' => \$datdir,
+) or pod2usage();
+mkpath $datdir;
+my $dumper = do {
+    if ($format eq 'YAML') {
+        require YAML;
+        sub { YAML::Dump(@_) }
+    } elsif ($format eq 'JSON') {
+        require JSON;
+        sub { JSON::encode_json(@_) }
+    } else {
+        print "Unknown format: $format";
+        pod2usage();
+    }
+};
 
 my $pluggable = Module::Pluggable::Object->new(
     'require' => 'yes',
     'search_path' => ['WWW::MobileCarrierJP'],
 );
 
-my $datdir = File::Spec->catfile($FindBin::Bin, '..', 'dat');
-mkdir $datdir;
 for my $module ($pluggable->plugins()) {
     next if $module eq 'WWW::MobileCarrierJP::Declare';
+
+    print "processing $module\n";
 
     my $fname = $module;
     $fname =~ s/^WWW::MobileCarrierJP:://;
     $fname =~ s/::/-/g;
     $fname = lc $fname;
-    YAML::DumpFile(
-        File::Spec->catfile($datdir, "$fname.yaml"),
-        $module->scrape(),
-    );
+
+    my $ofname = File::Spec->catfile($datdir, "$fname." . lc($format));
+    open my $fh, '>:utf8', $ofname;
+    print { $fh } $dumper->($module->scrape());
 }
+
+__END__
+
+=head1 SYNOPSIS
+
+    % www-mobilecarrierjp-dump_all.pl --format=JSON --datdir=/path/to/output
 
