70221ba9a6f3625da98ce16099e9f2587e43983e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-26 15:46:29 +0900

    ThirdForce::UserAgent は、DEPRECATED なので、Softbank の方にうつす必要がない

--- lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
+++ lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
@@ -1,4 +1,43 @@
 package WWW::MobileCarrierJP::ThirdForce::UserAgent;
-use parent qw/WWW::MobileCarrierJP::Softbank::UserAgent/;
+use WWW::MobileCarrierJP::Declare;
+
+parse_one( 
+    urls => ['http://creation.mb.softbank.jp/terminal/index.html'],
+    xpath => q(//tr[@bgcolor="#FFFFFF"]/td[@rowspan="5"]/..),
+    scraper => scraper {
+        col 1 => 'model'      => 'TEXT';
+        col 2 => 'user_agent' => ['TEXT', sub { s/\s+$//; }];
+    },
+);
+
 1;
-# THIS IS PLACEHOLDER FOR COMPATIBILITY.
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::MobileCarrierJP::ThirdForce::UserAgent - get UserAgent informtation from Softbank site(OBSOLETE)
+
+=head1 SYNOPSIS
+
+    (OBSOLETE)
+
+=head1 DESCRIPTION
+
+THIS MODULE IS OBSOLETE.
+
+YOU SHOULD USE Softbank::HTTPHeader & Softbank::Service.
+
+This module is no longer supported.
+
+You should also strongly avoid the use of this module.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno < tokuhirom gmail com >
+
+=head1 SEE ALSO
+
+L<WWW::MobileCarrierJP>
+
