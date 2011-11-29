5b2e95e2c60e753f76add0d1c3b041f36d61252b
Keiji, Yoshimi <walf443@gmail.com>
2011-11-30 07:42:49 +0900

    ラベルにマッチさせていると、変更させやすいと思われるので、idにマッチさせてみます

--- lib/WWW/MobileCarrierJP/EZWeb/CIDR.pm
+++ lib/WWW/MobileCarrierJP/EZWeb/CIDR.pm
@@ -14,7 +14,7 @@ sub scrape {
     for my $line ( split /\n/, $content ) {
         next if $skip_flag;
 
-        if ( $line =~ m{\Qalt="(3) PCSVサーバ"\E} ) {
+        if ( $line =~ m{id="PcsvSummer"} ) {
             $skip_flag++;
         } else {
             $body .= $line;
